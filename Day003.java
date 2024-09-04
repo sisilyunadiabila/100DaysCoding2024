7d0a383aad46876075ce8816da9381719cb2c195
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-04 22:19:03 +0800

    system.out.println

---
+++ Day003.java
@@ -0,0 +1,14 @@
+
+package day003;
+
+
+public class Day003 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("nama : sisil yunadiabila sb");
+        System.out.println("nim : D0224522");
+        System.out.println("kelas : Informatika D");
+    }
+    
+}

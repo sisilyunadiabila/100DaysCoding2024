71d6cebfa2ec0adbd23e4730c2fa384ca318dfac
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-05 23:05:25 +0800

    Day004

---
+++ Day004.java
@@ -0,0 +1,21 @@
+
+package day004;
+
+public class Day004 {
+
+   
+    public static void main(String[] args) {
+          String a = "hi i'm sisil imut";
+        String b = "i really want to be fluent in coding";
+        System.out.printf("%s %s",a,b);
+            
+        
+        
+        System.out.print("hi saya sisil");
+        System.out.print("i like going around the city");
+        System.out.print("i like drinking lemon tea");
+        System.out.print("i like watching the sunset");
+        System.out.println("and i like watching the sunset while drinking lemon tea");
+    }
+    
+}

4022f0131fa981591c34a76bd97c9f67fa107699
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-05 21:57:50 +0800

    Day003

--- Day003.java
+++ Day003.java
@@ -6,9 +6,37 @@ public class Day003 {
 
     
     public static void main(String[] args) {
-        System.out.println("nama : sisil yunadiabila sb");
-        System.out.println("nim : D0224522");
-        System.out.println("kelas : Informatika D");
+        System.out.println("Belum ada satu bulan\n" +
+"Ku yakin masih ada sisa wangiku di bajumu\n" +
+"Namun, kau tampak baik saja\n" +
+"Bahkan senyummu lebih lepas\n" +
+"Sedang aku di sini hampir gila\n" +
+"Kita tak temukan jalan\n" +
+"Sepakat akhiri setelah beribu debat panjang\n" +
+"Namun kau tampak baik saja\n" +
+"Bahkan senyummu lebih lepas\n" +
+"Sedang aku di sini belum terima\n" +
+"Bohongkah tangismu sore itu di pelukku?\n" +
+"Nyatanya pergiku pun tak lagi mengganggumu\n" +
+"Apa sudah ada kabar lain yang kautunggu?\n" +
+"Sudah adakah yang gantikanku\n" +
+"Yang khawatirkanmu setiap waktu\n" +
+"Yang cerita tentang apa pun sampai hal-hal tak perlu\n" +
+"Kalau bisa, jangan buru-buru\n" +
+"Kalau bisa, jangan ada dulu\n" +
+"Baru lewat satu bulan\n" +
+"Kemarin ulang tahunku tak ada pesan darimu\n" +
+"Tak apa, mungkin kau lupa\n" +
+"Atau sudah ada hati yang harus kaujaga\n" +
+"Sudah adakah yang gantikanku\n" +
+"Yang kauantar-jemput setiap Sabtu\n" +
+"Yang selalu ingatkan untuk pakai sabuk pengamanmu\n" +
+"Kalau bisa, jangan buru-buru\n" +
+"Sudah adakah yang gantikanku\n" +
+"Yang khawatirkanmu setiap waktu\n" +
+"Yang cerita tentang apa pun sampai hal-hal tak perlu\n" +
+"Kalau bisa, jangan buru-buru\n" +
+"Kalau bisa, jangan ada dulu");
     }
     
 }

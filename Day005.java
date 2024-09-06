10bb6419599b91ed4603a91e58158302868d413d
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-06 23:31:47 +0800

    Day005

---
+++ Day005.java
@@ -0,0 +1,15 @@
+/
+package day005;
+
+
+public class Day005 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("sisil");
+        System.out.println("call me sayang aja");
+        System.out.println("happy to join this organization");
+        System.out.println("that is all and thank you");
+    }
+    
+}

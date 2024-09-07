c47c75d101c83ae6a4452a936da033c975ac85f9
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-07 23:24:15 +0800

    Day006

---
+++ Day006.java
@@ -0,0 +1,20 @@
+
+package day006;
+
+
+public class Day006 {
+
+}
+
+    
+    public static void main(String[] args) {
+           String kelas,fakultas;
+        kelas = "D";
+        fakultas = "teknk";
+        
+        
+        System.out.println(kelas);
+        System.out.println(fakultas);
+}
+
+

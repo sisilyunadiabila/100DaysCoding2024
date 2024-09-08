c0691965f66db226abf6ed048b8b18fc27020fa5
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-08 16:13:42 +0800

    Day007

---
+++ Day007.java
@@ -0,0 +1,19 @@
+
+package day007;
+
+
+public class Day007 {
+
+   
+    public static void main(String[] args) {
+        // variabel inisialisasi
+        int age = 18;
+        boolean isActive = true;
+        
+        
+        System.out.println(age);
+        System.out.println(isActive);
+        
+    }
+    
+}

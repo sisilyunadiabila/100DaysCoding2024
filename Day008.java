d59afc49526bb15959dd0b4647c40dcc128ccc7e
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-09 22:10:27 +0800

    Day008

---
+++ Day008.java
@@ -0,0 +1,18 @@
+
+package day008;
+
+
+public class Day008 {
+
+   
+    public static void main(String[] args) {
+        // Variable Update 
+        System.out.println("nilai awal + angka");
+        
+         System.out.println("point awal:" +point);
+        point = 1;
+      
+        
+    }
+    
+}

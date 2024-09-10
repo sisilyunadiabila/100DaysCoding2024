859695f844716762f88aa48df3e0cf40a34e92f3
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-10 23:51:57 +0800

    Day009

---
+++ Day009.java
@@ -0,0 +1,15 @@
+
+package day009;
+
+
+public class Day009 {
+
+    
+    public static void main(String[] args) {
+        // fariabel konstanta
+        final float nilai = 2.7f;
+        final int jumlah = 227;
+           // jumlah = 222;  (akan menyebabkan error)
+    }
+    
+}

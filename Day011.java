d2004bad6422ff51e0ea5dbe0b7ae9d241bf1064
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-12 22:26:29 +0800

    Day011

---
+++ Day011.java
@@ -0,0 +1,14 @@
+
+package day011;
+
+
+public class Day011 {
+
+    
+    public static void main(String[] args) {
+         //membuat tipe data short
+        
+        System.out.println("Results perjumlahan a dan b adalah: " + results );
+    }
+    
+}

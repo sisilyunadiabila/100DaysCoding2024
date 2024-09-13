e4a3650991ae4b9d71e960cc78ef9611cbdcdd22
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-13 21:10:51 +0800

    Day012

---
+++ Day012.java
@@ -0,0 +1,17 @@
+
+package day012;
+
+
+public class Day012 {
+
+    
+    public static void main(String[] args) {
+       int bulan = 06;
+       int tahun = 2006;
+       
+        System.out.println("bulan:+ bulan");
+        System.out.println("tahun:+ tahun");
+       
+    }
+    
+}

970824a87cffb3ea4a10b4753f3b0533d7038c98
sisilyunadiabila <sisilyunadiabilasb@gmail.com>
2024-09-14 21:19:50 +0800

    tugas

---
+++ Day013.java
@@ -0,0 +1,25 @@
+
+package day013;
+
+
+public class Day013 {
+
+   
+    public static void main(String[] args) {
+       // membuat variable dengan tipe data berbeda 
+       int umur = 18;
+       short tanggal = 21;
+       
+        System.out.println("umur saya:" + umur);
+        
+       //update
+       umur = 18;
+       System.out.println("umur saya saat ini:" + umur);
+       System.out.printf("tanggal:" + tanggal);
+       
+       
+       
+               
+    }
+    
+}
